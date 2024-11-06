0267b0fca14182b749e91145055f539ad3761fbd
Edrick Koda <z5349845@unsw.edu.au>
2024-11-06 22:57:08 +1100

    finished authenticationn init

--- user.py
+++ user.py
@@ -1,6 +1,39 @@
+import os
+
 class AuthServer:
+
     def __init__(self):
+        credentials_filename='credentials.txt'
+        self.credentials = {}
+
         # loads the file of credentials
-        # breaks them down
-        # puts all of them into a hashmap
+        cwd = os.getcwd()
+        try:
+            with open(cwd + credentials_filename, 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    contents = line.split(' ')
+
+                    if len(contents) != 2:
+                        print("Invalid Credentials Detected")
+                        raise
+
+                    # need to validate the entries
+                    username, password = contents
+                    if not self.is_valid_credential(username) or not self.is_valid_credential(password):
+                        print("Invalid Credentials Detected")
+                        raise
+
+                    # add it into the hash map
+                    self.credentials[username] = password
+
+        except FileNotFoundError:
+            print("Credentials File not found.")
+            raise
+        except IOError:
+            print("Error reading file contents.")
+            raise
         return
+
+    def is_valid_credential(text: str):
+        return text.isprintable() and len(text) <= 16
